0d07797f65675c4260a96780cc43a7c0a7b42f5e
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-09-28 15:49:34 -0400

    feat: creates migration of categories and tasks_categories tables

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # back-end
 
+## Initial Project Basis
+```
+https://www.youtube.com/watch?v=Fbu7z5dXcRs
+```
 ## Project setup
+```
+npm install
+```
+
+### Start local project
 ```
 npm run dev
 ```
---
+++ src/database/migrations/20230928193239-create-tasks_categories.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize').Sequelize } */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks_categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      task_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'tasks',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks_categories');
+  }
+};
