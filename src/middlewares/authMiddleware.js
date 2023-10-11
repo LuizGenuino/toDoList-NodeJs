489c6d8586fc2c60d110e3614c01feaf08fc2b28
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 15:59:39 -0400

    feat: create auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+const { UnauthorizedError, NotFoundError } = require("../helpers/ApiError")
+const jwt = require('jsonwebtoken')
+const User = require("../models/User")
+
+module.exports = {
+    async authMiddleware(req, res, next) {
+        try {
+            const { authorization } = req.headers
+
+            if (!authorization) {
+                return next(new UnauthorizedError('Não Autorizado!'))
+                //throw new UnauthorizedError('Não Autorizado!')
+            }
+
+            const token = authorization.split(' ')[1]
+
+            const { id } = jwt.verify(token, process.env.JWT_PASSWORD ?? '')
+
+            const user = await User.findByPk(id,{
+                include: {association: 'tasks'}
+            })
+
+
+            if (!user) {
+                return next(new NotFoundError('Usuario não Encontrado'))
+                // throw new UnauthorizedError('Não Autorizado!')
+            }
+
+            req.user = user
+
+            next()
+        } catch (error) {
+            // Se ocorrer um erro na verificação do token, capture a exceção
+            // e retorne uma resposta de erro personalizada
+            return next(new UnauthorizedError('Token inválido'));
+        }
+
+    }
+}
