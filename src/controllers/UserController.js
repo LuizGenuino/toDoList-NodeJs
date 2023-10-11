269b86fd3116fed92fcfe5bf8362af966cfe79cb
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 16:20:05 -0400

    remove login function from user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -29,33 +29,4 @@ module.exports = {
         return res.status(200).json(users)
     },
 
-    async login(req, res, next) {
-        const { email, password } = req.body;
-
-        const user = await User.findOne({
-            where: { email }
-        })
-
-        if (!user) {
-            return next(new BadRequestError('Email não Cadastrado!'));//o next passa a exeção para frente (onde a função é chamada) e continua o codigo
-        }
-        
-
-        const verifyPassword = await bcrypt.compare(password, user.password)
-
-        if(!verifyPassword){
-            return next(new BadRequestError('Senha Incorreta'));//o next passa a exeção para frente (onde a função é chamada) e continua o codigo
-        }
-
-        const token = jwt.sign({id: user.id}, process.env.JWT_PASSWORD ?? '', {expiresIn: '1d'})
-
-        const { password: _, ...userWithoutPass } = user.dataValues // estou passando pra constante 'user' todos os dados da 'newUser' menos a 'password'
-
-        return res.status(200).json({
-            user: userWithoutPass,
-            token: 'Bearer '+token
-        })
-    },
-
-
 }
