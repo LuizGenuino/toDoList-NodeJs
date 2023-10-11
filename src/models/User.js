e19345ab1c2596c98cd3f159f6b0fd0d6e8ff119
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 15:56:37 -0400

    fix: changes the value of allowNull in the user model to false

--- src/models/User.js
+++ src/models/User.js
@@ -9,22 +9,22 @@ class User extends Model {
             },
             email: {
                 type: DataTypes.STRING,
-                allowNull: true,
+                allowNull: false,
                 unique: true, // Torna o campo "email" único
             },
             cellphone: {
                 type: DataTypes.STRING(13), // Limita a 13 caracteres
-                allowNull: true,
+                allowNull: false,
                 unique: true, // Torna o campo "cellphone" único
             },
             cpf: {
                 type: DataTypes.STRING(11), // Limita a 11 caracteres
-                allowNull: true,
+                allowNull: false,
                 unique: true, // Torna o campo "cpf" único
             },
             birthday: {
                 type: DataTypes.DATE,
-                allowNull: true,
+                allowNull: false,
             },
             password: {
                 type: DataTypes.STRING,
