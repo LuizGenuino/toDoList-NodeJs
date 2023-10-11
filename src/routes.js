02f5400de94b79e925214674e75c73659c6c46dc
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 16:20:59 -0400

    refactor: modify login route controller

--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,23 @@ const express = require('express')
 const UserController = require('./controllers/UserController')
 const TaskController = require('./controllers/TaskController');
 const CategoryController = require('./controllers/CategoryController');
-const ReportController = require('./controllers/ReportController');
 const { authMiddleware } = require('./middlewares/authMiddleware');
+const LoginController = require('./controllers/LoginController');
 
 const routes = express.Router()
 
-routes.post('/users', UserController.create);
-routes.post('/login', UserController.login);
+routes.post('/login', LoginController.login);
 
+
+routes.post('/users', UserController.create);
 routes.use(authMiddleware) // significa que todas as rotas abaixo utilizaram o authMiddleware
 // uma alternativa para não colocar em cada uma das rotas. EX: routes.get('/users', authMiddleware, UserController.list);
-
 routes.get('/users', UserController.list);
 
 
 routes.post('/tasks', TaskController.create)
 routes.get('/tasks', TaskController.list);
+
 
 routes.post('/categories/:task_id', CategoryController.create)
 routes.get('/categories/:task_id', CategoryController.list);
