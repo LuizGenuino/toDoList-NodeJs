0c26e18e20751491f7753d48cdbf086dbaaebbb3
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 16:04:54 -0400

    refactor: modify create and list function for use the auth middleware

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,31 +1,18 @@
 const Task = require('../models/Task');
 const User = require('../models/User');
 
+
 module.exports = {
     async create(req, res) {
-        const { user_id } = req.params;
         const { title, subtitle, concluded, preference } = req.body;
 
-        const user = await User.findByPk(user_id, {
-            include: {association: 'tasks'}
-        })
-
-        if(!user){
-            return res.status(400).json({error: 'User Not Found'});
-        }
-
-        const tasks = await Task.create({title, subtitle, concluded, preference, user_id})
+        const tasks = await Task.create({title, subtitle, concluded, preference, user_id: req.user.id})
 
         return res.status(200).json(tasks)
     },
 
-    async list(req, res) {
-        const { user_id } = req.params;
-
-        const user = await User.findByPk(user_id, {
-            include: {association: 'tasks'}
-        })
-
-        return res.status(200).json(user.tasks)
+    async list(req, res, next) {
+        // como a função de validação do token no "authMiddleware" ja retornar os dados que precisamos apenas retornamos os dados aqui
+        return res.status(200).json(req.user.tasks)
     }
 }
