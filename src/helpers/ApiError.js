78ef3a3688675599eca5ae15a60783885067d777
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 16:00:36 -0400

    refactor: modify function export to class export

--- src/helpers/ApiError.js
+++ src/helpers/ApiError.js
@@ -1,45 +1,31 @@
-
 class ApiError extends Error {
-    statusCode
-
     constructor(message, statusCode) {
-        super(message)
-        this.statusCode = statusCode
+        super(message);
+        this.statusCode = statusCode;
     }
 }
 
 class BadRequestError extends ApiError {
     constructor(message) {
-        super(message, 400)
+        super(message, 400);
     }
 }
 
 class NotFoundError extends ApiError {
     constructor(message) {
-        super(message, 404)
+        super(message, 404);
     }
 }
 
 class UnauthorizedError extends ApiError {
     constructor(message) {
-        super(message, 401)
+        super(message, 401);
     }
 }
 
 module.exports = {
-    ApiError(message, statusCode) {
-        return new ApiError(message, statusCode)
-    },
-    BadRequestError(message) {
-        return new BadRequestError(message)
-    },
-
-    NotFoundError(message) {
-        return new NotFoundError(message)
-    },
-    UnauthorizedError(message) {
-        return new UnauthorizedError(message)
-    }
-}
-
-
+    ApiError,
+    BadRequestError,
+    NotFoundError,
+    UnauthorizedError,
+};
