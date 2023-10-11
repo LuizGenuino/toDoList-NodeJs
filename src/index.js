f6e21351bc9f129406ab7ed4881190ed56fe80d5
LuizGenuino <luiz.gustavo.dev@gmail.com>
2023-10-11 15:58:22 -0400

    refactor: modify import of 'errorMiddleware'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const error = require('./middlewares/error')
+const {errorMiddleware} = require('./middlewares/error')
+require('dotenv').config()
 
 require('./database')
 
@@ -9,6 +10,9 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
-app.use(error.errorMiddleware)
+app.use(errorMiddleware)
+
+
+  
 
 app.listen(3333);
