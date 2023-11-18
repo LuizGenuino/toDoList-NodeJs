3c27347047455b83a0668528e1a2dd7bb7af8714
LuizGenuino <luizgustavo@pitom.com.br>
2023-11-18 00:25:57 -0400

    feat: add cors permition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 const {errorMiddleware} = require('./middlewares/error')
+const cors = require('cors');
+
 require('dotenv').config()
 
 require('./database')
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json())
 app.use(routes)
